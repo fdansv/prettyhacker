ff3756acddc18a49150016236af56bd89500ff60
Francisco Dans <fdansv@gmail.com>
2013-09-10 02:34:09 +0100

    Adding AsyncTask to load URL

--- PrettyHacker/src/main/java/com/dansd/PrettyHacker/MainNews.java
+++ PrettyHacker/src/main/java/com/dansd/PrettyHacker/MainNews.java
@@ -6,6 +6,16 @@ import android.app.Activity;
 import android.view.Menu;
 
 import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainNews extends Activity {
 
@@ -13,6 +23,7 @@ public class MainNews extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new RequestTask().execute("http://news.ycombinator.com");
     }
 
 
@@ -23,15 +34,41 @@ public class MainNews extends Activity {
         return true;
     }
 
-    private class getTheNews extends AsyncTask<HttpRequest, >{
+    class RequestTask extends AsyncTask<String, String, String>{
 
         @Override
-        protected Object doInBackground(Object[] objects) {
-            return null;
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
         }
-    }
-    private HttpRequest prepareNewsRequest(){
 
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            System.out.println(result);
+
+            //Do anything with response..
+        }
     }
     
 }
