ec3fe5e9d4fcfdb6d6c732446141bd34e94a9602
Francisco Dans <fdansv@gmail.com>
2013-09-10 17:42:06 +0100

    Adde some kind of soup library to parse DOM

--- PrettyHacker/src/main/java/com/dansd/PrettyHacker/MainNews.java
+++ PrettyHacker/src/main/java/com/dansd/PrettyHacker/MainNews.java
@@ -5,19 +5,15 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class MainNews extends Activity {
+
+    private Document newsDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +30,41 @@ public class MainNews extends Activity {
         return true;
     }
 
-    class RequestTask extends AsyncTask<String, String, String>{
+    class RequestTask extends AsyncTask<String, Document, Document> {
 
         @Override
-        protected String doInBackground(String... uri) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response;
-            String responseString = null;
+        protected Document doInBackground(String... uri) {
             try {
-                response = httpclient.execute(new HttpGet(uri[0]));
-                StatusLine statusLine = response.getStatusLine();
-                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    response.getEntity().writeTo(out);
-                    out.close();
-                    responseString = out.toString();
-                } else{
-                    //Closes the connection.
-                    response.getEntity().getContent().close();
-                    throw new IOException(statusLine.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                //TODO Handle problems..
-            } catch (IOException e) {
-                //TODO Handle problems..
+                newsDoc = Jsoup.connect(uri[0]).get();
+                return newsDoc;
             }
-            return responseString;
+            catch (IOException e) {
+
+            }
+            return null;
+
         }
 
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(Document result) {
             super.onPostExecute(result);
-            System.out.println(result);
-
-            //Do anything with response..
+            if(result != null){
+                parseNewsDocument();
+            }
+            else{
+                dealWithNullDocument();
+            }
         }
     }
-    
+
+    private void dealWithNullDocument() {
+        System.out.println("Null doc");
+    }
+
+    private void parseNewsDocument() {
+        Elements articles = newsDoc.select("html body center table tbody");
+        System.out.println("Printing selection");
+        System.out.println(articles);
+    }
+
 }
